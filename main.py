3ae4a02d2c2b6d9337ca7339aa51bc055a4814df
unknown <oelkuctmp@gmail.com>
2024-01-22 14:49:32 -0500

    Added query functions for getting Employees by Dept

--- main.py
+++ main.py
@@ -5,22 +5,20 @@ from Alerts import Alerts
 from Manage_Employees import Employee_Management
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # -- Get phones of Employees opted in
-    empdf = pd.read_csv('data/emp.csv', names=['empid', 'name', 'phone', 'email', 'department'])
-    emp_manage = Employee_Management(empdf)
-    alertlist = emp_manage.alert_phones(empdf)
 
-    # -- Set thresholds
+    # -- Get phones of Employees opted in
+    emp_manage = Employee_Management('mpo', 'temp', 'company.server', 3052)
+    empdf = emp_manage.get_alert_employees()
+    alertlist = emp_manage.get_alert_phonenums(empdf)
+
+    # -- Set thresholds for current Product SPECs
     thresh = {'pH': [7.0, 8], 'DO': [40, 45]}
 
-    # -- Check Alerts to send (compare dataset with thresholds)
+    # -- Check Alerts to send (compare current trends with thresholds)
     alertsender = Alerts('mp', '123', '423', 'manuf')
-    alertstosend = alertsender.check_alert_threshold(thresh, 'data/biordf.csv')
+    alertstosend = alertsender.check_alert_threshold(thresh)
 
     # ---- Send Alert Messages
-
     alertsender.set_alert_msg(alertstosend, 2402851931, 12345)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
